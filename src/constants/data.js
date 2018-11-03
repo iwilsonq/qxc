aa9fdbdfdc3f680d8f01fc79e879657761281968
Ian Wilson <iwilsonq@gmail.com>
2018-11-03 16:56:55 -0700

    add pieces service

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,111 +1,126 @@
-const pieces = [
+export const MOCK_PIECES = [
   {
+    id: '1',
     title: 'Ancient cave painting',
     artMovement: 'bce',
   },
 
   {
+    id: '2',
     title: 'Venus of Willendorf',
     artMovement: 'bce',
   },
 
   {
+    id: '3',
     title: 'King Edgar',
     artMovement: 'middle ages',
   },
 
   {
+    id: '4',
     title: 'Disk Thrower',
     artMovement: 'ancient greece',
   },
 
   {
+    id: '5',
     title: 'Birth of Venus by Botticelli',
     artMovement: 'Renaissance',
   },
 
   {
+    id: '6',
     title: 'Mona Lisa by Da Vinci',
     artMovement: 'Renaissance',
   },
 
   {
+    id: '7',
     title: 'Liberty Leading the people by Delacroix',
     artMovement: 'French Romanticism',
   },
 
   {
+    id: '8',
     title: 'The Swing by Jean-Honoré Fragonard',
     artMovement: 'French Romanticism',
   },
 
   {
+    id: '9',
     title: 'Saturn Devouring his Son by Goya',
     artMovement: 'Spanish Romanticism',
   },
 
   {
+    id: '10',
     title: 'The Great Wave by Hokusai',
     artMovement: 'ukiyo-e',
   },
 
   {
+    id: '11',
     title: 'The Scream by Munch',
     artMovement: 'impressionism',
   },
 
   {
+    id: '12',
     title: 'Violin and Grapes by Picasso',
     artMovement: 'Cubism',
   },
 
   {
+    id: '13',
     title: 'Untitled by Rothko',
     artMovement: 'Abstractionism',
   },
 
   {
+    id: '14',
     title: 'Sportsmen Figures by Malevich',
     artMovement: 'Russian suprematism',
   },
 
   {
+    id: '15',
     title: 'Campbell Soup by Warhol',
     artMovement: 'Pop art',
   },
 ]
 
-const galeries = [
+export const MOCK_GALLERIES = [
   {
-    galeryName: 'San Francisco Museum of Modern Art',
+    galleryName: 'San Francisco Museum of Modern Art',
     address: '151 3rd St, San Francisco, CA 94103',
   },
   {
-    galeryName: 'International Art Museum of America',
+    galleryName: 'International Art Museum of America',
     address: '1023 Market St, San Francisco, CA 94103',
   },
   {
-    galeryName: 'de Young Fine Arts Museum',
+    galleryName: 'de Young Fine Arts Museum',
     address: '50 Hagiwara Tea Garden Dr, San Francisco, CA 94118',
   },
   {
-    galeryName: 'Asian Art Museum',
+    galleryName: 'Asian Art Museum',
     address: '200 Larkin St, San Francisco, CA 94102',
   },
   {
-    galeryName: 'Fine Arts Museums of San Francisco',
+    galleryName: 'Fine Arts Museums of San Francisco',
     address: '50 Hagiwara Tea Garden Dr, San Francisco, CA 9411',
   },
   {
-    galeryName: 'Cartoon Art Museum',
+    galleryName: 'Cartoon Art Museum',
     address: '781 Beach St 1st floor, San Francisco, CA 94109',
   },
   {
-    galeryName: 'Museum of Craft and Design',
+    galleryName: 'Museum of Craft and Design',
     address: '2569 3rd St, San Francisco, CA 94107',
   },
   {
-    galeryName: 'San Francisco Art Exchange',
+    galleryName: 'San Francisco Art Exchange',
     address: '458 Geary St, San Francisco, CA 94102',
   },
 ]
