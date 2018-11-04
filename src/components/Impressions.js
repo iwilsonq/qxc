2010831630edb909fab986173b2eaa4ec7d29802
Roman Morozov <sublimeye.ua@gmail.com>
2018-11-04 11:52:41 -0800

    add fake mock data

--- src/components/Impressions.js
+++ src/components/Impressions.js
@@ -1,6 +1,45 @@
 import React, { Component } from 'react'
 import styled from 'react-emotion'
 
+const galleries = [
+  {
+    galleryId: '123',
+    title: 'Grand Gallery',
+    fullAddress: '1238 Somestreet, San Francisco, CA',
+    score: 3, // liked the most art from that gallery
+
+    lat: 37.7749,
+    lng: 122.4194,
+  },
+  {
+    galleryId: '124',
+    title: 'Picasso Arts',
+    fullAddress: '121 Folsom street, San Francisco, CA',
+    score: 1, // liked only 1 piece
+
+    lat: 37.2749,
+    lng: 122.6194,
+  },
+  {
+    galleryId: '124',
+    title: 'Picasso Arts',
+    fullAddress: '121 Folsom street, San Francisco, CA',
+    score: 0, // just a suggestion
+
+    lat: 37.2749,
+    lng: 122.6194,
+  },
+  {
+    galleryId: '124',
+    title: 'Picasso Arts',
+    fullAddress: '121 Folsom street, San Francisco, CA',
+    score: 0,
+
+    lat: 37.2749,
+    lng: 122.6194,
+  },
+]
+
 export class Impressions extends Component {
   render() {
     return (
