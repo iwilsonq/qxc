dc57aa79618a92373234a583efebee750df69d21
Simon Baev <simonbaev@gmail.com>
2018-11-03 18:22:15 -0700

    first draft of exhibit

--- src/components/Exhibit.vue
+++ src/components/Exhibit.vue
@@ -1,8 +1,15 @@
 <template>
-  <div v-if="activePiece" class="container">
+  <div v-if="activeURL" class="container">
     <img :src="activeURL" class="image">
     <div class="buttons">
-
+      <button @click="clickHandler(false)" class="button is-dark is-large button--no">
+          <!-- <b-icon pack="fas" icon="check"></b-icon> -->
+          <span>No</span>
+      </button>
+      <button @click="clickHandler(true)" class="button is-dark is-large button--yes">
+          <!-- <b-icon pack="fas" icon="check"></b-icon> -->
+          <span>Yes</span>
+      </button>
     </div>
   </div>
 </template>
@@ -15,7 +22,7 @@ export default {
   name: 'exhibit',
   data: () => ({
     pieces: [],
-    activePiece: null
+    index: 0
   }),
   props: {
     fetchPieces: {
@@ -26,13 +33,37 @@ export default {
       type: Function,
       default: () => {},
     },
+    navigate: {
+      type: Function,
+      default: () => {},
+    },
   },
   computed: {
+    activePiece() {
+      return this.pieces[this.index]
+    },
     activeURL() {
-      return getUrl(this.activePiece)
+      return getUrl(this.activePiece || { title: 'boo' })
+    }
+  },
+  methods: {
+    clickHandler(liked) {
+      const answer = {}
+      answer[this.activePiece.id] = liked
+      if(this.submitAnswer(answer)) {
+        this.index = (this.index + 1) % this.pieces.length
+        if(!this.index) {
+          // all images displayed and need more
+          this.fetchPieces().then(pieces => this.pieces = pieces)
+        }
+      }
+      else {
+        this.navigate('/impressions')
+      }
     }
   },
   mounted() {
+    this.index = 0
     this.fetchPieces().then(pieces => this.pieces = pieces)
   },
 }
@@ -40,14 +71,18 @@ export default {
 
 <style>
 .container {
+  width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 }
-.my-list {
-  list-style: none;
+.buttons {
+  width: 925px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
 
