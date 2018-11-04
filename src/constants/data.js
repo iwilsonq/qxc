62a87c4869ae585029eda2db3cf3af2dd7c5f5bb
Roman Morozov <sublimeye.ua@gmail.com>
2018-11-03 19:08:47 -0700

    feat(fauna) init setup, keys

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,126 +1,110 @@
 export const MOCK_PIECES = [
   {
-    id: '1',
     title: 'Ancient cave painting',
-    artMovement: 'bce',
+    movement: 'bce',
   },
 
   {
-    id: '2',
     title: 'Venus of Willendorf',
-    artMovement: 'bce',
+    movement: 'bce',
   },
 
   {
-    id: '3',
     title: 'King Edgar',
-    artMovement: 'middle ages',
   },
 
   {
-    id: '4',
     title: 'Disk Thrower',
-    artMovement: 'ancient greece',
+    movement: 'ancient greece',
   },
 
   {
-    id: '5',
     title: 'Birth of Venus by Botticelli',
-    artMovement: 'Renaissance',
+    movement: 'Renaissance',
   },
 
   {
-    id: '6',
     title: 'Mona Lisa by Da Vinci',
-    artMovement: 'Renaissance',
+    movement: 'Renaissance',
   },
 
   {
-    id: '7',
     title: 'Liberty Leading the people by Delacroix',
-    artMovement: 'French Romanticism',
+    movement: 'French Romanticism',
   },
 
   {
-    id: '8',
     title: 'The Swing by Jean-Honoré Fragonard',
-    artMovement: 'French Romanticism',
+    movement: 'French Romanticism',
   },
 
   {
-    id: '9',
     title: 'Saturn Devouring his Son by Goya',
-    artMovement: 'Spanish Romanticism',
+    movement: 'Spanish Romanticism',
   },
 
   {
-    id: '10',
     title: 'The Great Wave by Hokusai',
-    artMovement: 'ukiyo-e',
+    movement: 'ukiyo-e',
   },
 
   {
-    id: '11',
     title: 'The Scream by Munch',
-    artMovement: 'impressionism',
+    movement: 'impressionism',
   },
 
   {
-    id: '12',
     title: 'Violin and Grapes by Picasso',
-    artMovement: 'Cubism',
+    movement: 'Cubism',
   },
 
   {
-    id: '13',
     title: 'Untitled by Rothko',
-    artMovement: 'Abstractionism',
+    movement: 'Abstractionism',
   },
 
   {
-    id: '14',
     title: 'Sportsmen Figures by Malevich',
-    artMovement: 'Russian suprematism',
+    movement: 'Russian suprematism',
   },
 
   {
-    id: '15',
     title: 'Campbell Soup by Warhol',
-    artMovement: 'Pop art',
+    movement: 'Pop art',
   },
 ]
 
 export const MOCK_GALLERIES = [
   {
-    galleryName: 'San Francisco Museum of Modern Art',
-    address: '151 3rd St, San Francisco, CA 94103',
+    title: 'San Francisco Museum of Modern Art',
+    fullAddress: '151 3rd St, San Francisco, CA 94103',
   },
   {
-    galleryName: 'International Art Museum of America',
-    address: '1023 Market St, San Francisco, CA 94103',
+    title: 'International Art Museum of America',
+    fullAddress: '1023 Market St, San Francisco, CA 94103',
   },
   {
-    galleryName: 'de Young Fine Arts Museum',
-    address: '50 Hagiwara Tea Garden Dr, San Francisco, CA 94118',
+    title: 'de Young Fine Arts Museum',
+    fullAddress: '50 Hagiwara Tea Garden Dr, San Francisco, CA 94118',
   },
   {
-    galleryName: 'Asian Art Museum',
-    address: '200 Larkin St, San Francisco, CA 94102',
+    title: 'Asian Art Museum',
+    fullAddress: '200 Larkin St, San Francisco, CA 94102',
   },
   {
-    galleryName: 'Fine Arts Museums of San Francisco',
-    address: '50 Hagiwara Tea Garden Dr, San Francisco, CA 9411',
+    title: 'Fine Arts Museums of San Francisco',
+    fullAddress: '50 Hagiwara Tea Garden Dr, San Francisco, CA 9411',
   },
   {
-    galleryName: 'Cartoon Art Museum',
-    address: '781 Beach St 1st floor, San Francisco, CA 94109',
+    title: 'Cartoon Art Museum',
+    fullAddress: '781 Beach St 1st floor, San Francisco, CA 94109',
   },
   {
-    galleryName: 'Museum of Craft and Design',
-    address: '2569 3rd St, San Francisco, CA 94107',
+    title: 'Museum of Craft and Design',
+    fullAddress: '2569 3rd St, San Francisco, CA 94107',
   },
   {
-    galleryName: 'San Francisco Art Exchange',
-    address: '458 Geary St, San Francisco, CA 94102',
+    title: 'San Francisco Art Exchange',
+    fullAddress: '458 Geary St, San Francisco, CA 94102',
   },
 ]
