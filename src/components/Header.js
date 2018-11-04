bf4c9d5fb498c940a93e4699630ae9d8d24b7173
Vadim Dermanovskiy <thenoodly@gmail.com>
2018-11-03 20:31:25 -0700

    remade header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 
 import styled from 'react-emotion'
 
 import netlifyIdentity from 'netlify-identity-widget'
 
-const toggleLogin = e => {
-  e.preventDefault()
+export class Header extends Component {
+  componentDidMount() {
+    netlifyIdentity.init()
+    netlifyIdentity.on('init', user => console.log('init', user))
+    // netlifyIdentity.on('login', user => this.setState({ user }, loginUser()))
+  }
 
-  netlifyIdentity.open()
+  toggleLogin = e => {
+    e.preventDefault()
+
+    netlifyIdentity.open()
+  }
+  loginUser = () => {
+    debugger
+  }
+  render() {
+    const { siteTitle } = this.props
+    return (
+      <Wrap>
+        <div className="container">
+          <LinkContainer>
+            <h1 style={{ margin: 0 }}>
+              <MainLink to="/">{siteTitle}</MainLink>
+            </h1>
+            <div>
+              <LoginButton onClick={this.toggleLogin} className="button">
+                Login
+              </LoginButton>
+            </div>
+          </LinkContainer>
+        </div>
+      </Wrap>
+    )
+  }
 }
-
-const Header = ({ siteTitle }) => (
-  <Wrap>
-    <div className="container">
-      <LinkContainer>
-        <h1 style={{ margin: 0 }}>
-          <MainLink to="/">{siteTitle}</MainLink>
-        </h1>
-        <LoginButton onClick={toggleLogin} className="button">
-          Login
-        </LoginButton>
-      </LinkContainer>
-    </div>
-  </Wrap>
-)
 
 export default Header
 
@@ -36,6 +51,7 @@ const LinkContainer = styled('div')`
   display: flex;
   justify-content: space-between;
   width: 100%;
+  padding: 0 15px;
 `
 
 const MainLink = styled(Link)`
