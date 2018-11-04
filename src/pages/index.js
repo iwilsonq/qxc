677dc006888a56563259d079ae2d303a9ccb7820
Ian Wilson <iwilsonq@gmail.com>
2018-11-04 12:27:06 -0800

    form component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import EmailForm from '../components/Form'
 import LandingPage from '../components/LandingPage'
 
 const IndexPage = () => (
   <Layout>
     <LandingPage />
+    <EmailForm />
   </Layout>
 )
 
