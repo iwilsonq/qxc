f8b5ae4f731397b1b22d21b949c2ed0ef9f6c1d2
Vadim Dermanovskiy <thenoodly@gmail.com>
2018-11-04 11:51:46 -0800

    removed netlify init

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react'
 import { Link } from 'gatsby'
 import styled from 'react-emotion'
 
-import netlifyIdentity from 'netlify-identity-widget'
-
 export class Header extends Component {
   componentDidMount() {
-    netlifyIdentity.init()
     netlifyIdentity.on('init', user => console.log('init', user))
     // netlifyIdentity.on('login', user => this.setState({ user }, loginUser()))
   }
