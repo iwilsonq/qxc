3ccb9ae315f7dd98a018e26619b3c5eaa8514e33
Simon Baev <simonbaev@gmail.com>
2018-11-04 15:04:01 -0800

    images replaced and edited

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,111 +1,136 @@
 export const MOCK_PIECES = [
   {
-    title: 'Ancient cave painting',
-    movement: 'bce',
+    "title": 'Ancient cave painting',
+    "movement": 'bce',
+    "url": "1.jpg"
   },
 
   {
-    title: 'Venus of Willendorf',
-    movement: 'bce',
+    "title": 'Venus of Willendorf',
+    "movement": 'bce',
+    "url": "2.jpg"
   },
 
   {
-    title: 'King Edgar',
-    movement: 'middle ages',
+    "title": 'King Edgar',
+    "movement": 'middle ages',
+    "url": "3.jpg"
   },
 
   {
-    title: 'Disk Thrower',
-    movement: 'ancient greece',
+    "title": 'Disk Thrower',
+    "movement": 'ancient greece',
+    "url": "4.jpg"
   },
 
   {
-    title: 'Birth of Venus by Botticelli',
-    movement: 'Renaissance',
+    "title": 'Birth of Venus by Botticelli',
+    "movement": 'Renaissance',
+    "url": "5.jpg"
   },
 
   {
-    title: 'Mona Lisa by Da Vinci',
-    movement: 'Renaissance',
+    "title": 'Mona Lisa by Da Vinci',
+    "movement": 'Renaissance',
+    "url": "6.jpg"
   },
 
   {
-    title: 'Liberty Leading the people by Delacroix',
-    movement: 'French Romanticism',
+    "title": 'Liberty Leading the people by Delacroix',
+    "movement": 'French Romanticism',
+    "url": "7.jpg"
   },
 
   {
-    title: 'The Swing by Jean-Honoré Fragonard',
-    movement: 'French Romanticism',
+    "title": 'The Swing by Jean-Honoré Fragonard',
+    "movement": 'French Romanticism',
+    "url": "8.jpg"
   },
 
   {
-    title: 'Saturn Devouring his Son by Goya',
-    movement: 'Spanish Romanticism',
+    "title": 'Saturn Devouring his Son by Goya',
+    "movement": 'Spanish Romanticism',
+    "url": "9.jpg"
   },
 
   {
-    title: 'The Great Wave by Hokusai',
-    movement: 'ukiyo-e',
+    "title": 'The Great Wave by Hokusai',
+    "movement": 'ukiyo-e',
+    "url": "10.jpg"
   },
 
   {
-    title: 'The Scream by Munch',
-    movement: 'impressionism',
+    "title": 'The Scream by Munch',
+    "movement": 'impressionism',
+    "url": "11.jpg"
   },
 
   {
-    title: 'Violin and Grapes by Picasso',
-    movement: 'Cubism',
+    "title": 'Violin and Grapes by Picasso',
+    "movement": 'Cubism',
+    "url": "12.jpg"
   },
 
   {
-    title: 'Untitled by Rothko',
-    movement: 'Abstractionism',
+    "title": 'Untitled by Rothko',
+    "movement": 'Abstractionism',
+    "url": "13.jpg"
   },
 
   {
-    title: 'Sportsmen Figures by Malevich',
-    movement: 'Russian suprematism',
+    "title": 'Sportsmen Figures by Malevich',
+    "movement": 'Russian suprematism',
+    "url": "14.jpg"
   },
 
   {
-    title: 'Campbell Soup by Warhol',
-    movement: 'Pop art',
+    "title": 'Campbell Soup by Warhol',
+    "movement": 'Pop art',
+    "url": "15.jpg"
   },
+  {
+    "title": 'Starry Night',
+    "movement": 'Post-impressionism',
+    "url": "15.jpg"
+  },
+  {
+    "title": 'Kiss',
+    "movement": 'Impressionism',
+    "url": "15.jpg"
+  }
 ]
 
 export const MOCK_GALLERIES = [
   {
-    title: 'San Francisco Museum of Modern Art',
+    "title": 'San Francisco Museum of Modern Art',
     fullAddress: '151 3rd St, San Francisco, CA 94103',
   },
   {
-    title: 'International Art Museum of America',
+    "title": 'International Art Museum of America',
     fullAddress: '1023 Market St, San Francisco, CA 94103',
   },
   {
-    title: 'de Young Fine Arts Museum',
+    "title": 'de Young Fine Arts Museum',
     fullAddress: '50 Hagiwara Tea Garden Dr, San Francisco, CA 94118',
   },
   {
-    title: 'Asian Art Museum',
+    "title": 'Asian Art Museum',
     fullAddress: '200 Larkin St, San Francisco, CA 94102',
   },
   {
-    title: 'Fine Arts Museums of San Francisco',
+    "title": 'Fine Arts Museums of San Francisco',
     fullAddress: '50 Hagiwara Tea Garden Dr, San Francisco, CA 9411',
   },
   {
-    title: 'Cartoon Art Museum',
+    "title": 'Cartoon Art Museum',
     fullAddress: '781 Beach St 1st floor, San Francisco, CA 94109',
   },
   {
-    title: 'Museum of Craft and Design',
+    "title": 'Museum of Craft and Design',
     fullAddress: '2569 3rd St, San Francisco, CA 94107',
   },
   {
-    title: 'San Francisco Art Exchange',
+    "title": 'San Francisco Art Exchange',
     fullAddress: '458 Geary St, San Francisco, CA 94102',
   },
 ]
