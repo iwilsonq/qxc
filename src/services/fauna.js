1a12af60ecc022e8ca60e16a623c57f951b87039
Roman Morozov <sublimeye.ua@gmail.com>
2018-11-04 16:36:48 -0800

    use lambdas instead

--- src/services/fauna.js
+++ src/services/fauna.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
-import faunadb, { query as q } from 'faunadb'
-import { makeFaunaGreatAgain } from '../helpers/fauna'
+// import faunadb, { query as q } from 'faunadb'
+// import { makeFaunaGreatAgain } from '../helpers/fauna'
 
-const client = new faunadb.Client({ secret: process.env.FAUNADB_SERVER_SECRET })
+// const client = new faunadb.Client({ secret: process.env.FAUNADB_SERVER_SECRET })
 
 export const fetchPieces = () => {
-  return client
-    .query(q.Map(q.Paginate(q.Match(q.Index('all_piece'))), x => q.Get(x)))
-    .then(res => res.data)
-    .then(pieces => pieces.map(makeFaunaGreatAgain))
-  // return axios('/.netlify/functions/exhibits')
+  // return client
+  //   .query(q.Map(q.Paginate(q.Match(q.Index('all_piece'))), x => q.Get(x)))
+  //   .then(res => res.data)
+  //   .then(pieces => pieces.map(makeFaunaGreatAgain))
+  return axios('/.netlify/functions/exhibits')
 }
 
 export const fetchImpressions = () => {
